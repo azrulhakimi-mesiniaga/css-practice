8edf8bd728cba4e6d9e2d337f6742933efadd592
azrulhakimi-mesiniaga <150011612+azrulhakimi-mesiniaga@users.noreply.github.com>
2024-10-21 10:08:54 +0800

    add icon transition

--- src/components/FloatingMenu.vue
+++ src/components/FloatingMenu.vue
@@ -14,12 +14,23 @@ watch(showMenu, (newValue) => {
 </script>
 
 <template>
-  <div
-    class="h-16 w-16 bg-red-500 rounded-full relative"
-    @mouseover="showMenu = true"
-    @mouseleave="showMenu = false"
-  >
-    <div class="absolute top-[-300px] flex flex-col gap-2">
+  <div class="relative">
+    <div
+      class="h-16 w-16 bg-red-500 rounded-full cursor-pointer shadow-md hover:shadow-lg hover:scale-105 transition-all duration-300 flex justify-center items-center"
+      @click="showMenu = !showMenu"
+    >
+      <transition name="icon" mode="out-in">
+        <div v-if="showMenu" key="close">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </div>
+        <div v-else key="settings">
+          <img src="../assets/settings.svg" alt="">
+        </div>
+      </transition>
+    </div>
+    <div class="absolute top-[-300px] flex flex-col gap-2 hover:scale-100">
       <transition-group name="menus">
         <div
           v-for="index in circles"
@@ -45,5 +56,14 @@ watch(showMenu, (newValue) => {
 .menus-enter-to, .menus-leave-from {
   opacity: 1;
   transform: translateY(0);
+}
+
+.icon-enter-active, .icon-leave-active {
+  transition: opacity 0.1s ease-in-out, transform 0.1s ease-in-out;
+}
+
+.icon-enter-from, .icon-leave-to {
+  opacity: 0;
+  transform: translateX(10px);
 }
 </style>
